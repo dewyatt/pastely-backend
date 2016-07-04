03af8f5ef666d293b3c4f42a9f8653c65e566495
Daniel Wyatt <Daniel.Wyatt@gmail.com>
2016-07-03 21:47:46 -0400

    Fix typo in deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,6 +8,6 @@ tag="gcr.io/pastely-1357/pastely-backend:${git_sha1}.${deploy_environment}.v${BU
 gcloud docker push "$tag"
 
 kubectl patch deployment pastely-backend \
-  --namespace=pastely-"${deploy_environment"} \
+  --namespace=pastely-"${deploy_environment}" \
   -p \
   '{"spec":{"template":{"spec":{"containers":[{"name":"pastely-backend","image":'"\"${tag}\""'}]}}}}'
