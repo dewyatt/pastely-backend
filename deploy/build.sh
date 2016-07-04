7183ff42387c01a16bd8be2e36430c71ab5339d1
Daniel Wyatt <daniel@dewyatt.com>
2016-07-03 21:35:25 -0400

    Add build and deploy scripts for jenkins. Adjust image tagging convention.

---
+++ deploy/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e -u
+
+deploy_environment="$1"
+git_sha1="$2"
+tag="gcr.io/pastely-1357/pastely-backend:${git_sha1}.${deploy_environment}.v${BUILD_NUMBER}"
+
+docker build \
+    --build-arg=deploy_environment="$deploy_environment" \
+    --build-arg=git_sha1="$git_sha1" \
+    -t "$tag" .
---
+++ deploy/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e -u
+
+deploy_environment="$1"
+git_sha1="$2"
+tag="gcr.io/pastely-1357/pastely-backend:${git_sha1}.${deploy_environment}.v${BUILD_NUMBER}"
+
+gcloud docker push "$tag"
+
+kubectl patch deployment pastely-backend \
+  --namespace=pastely-"${deploy_environment"} \
+  -p \
+  '{"spec":{"template":{"spec":{"containers":[{"name":"pastely-backend","image":'"\"${tag}\""'}]}}}}'
