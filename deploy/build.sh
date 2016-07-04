6d5b977cbdb334037fb509736a2e3b87a4655ebc
Daniel Wyatt <daniel@dewyatt.com>
2016-07-03 21:37:54 -0400

    Move to deploy directory before starting build, rather than having the Jenkinsfile do it.

--- deploy/build.sh
+++ deploy/build.sh
@@ -5,6 +5,8 @@ deploy_environment="$1"
 git_sha1="$2"
 tag="gcr.io/pastely-1357/pastely-backend:${git_sha1}.${deploy_environment}.v${BUILD_NUMBER}"
 
+cd deploy
+
 docker build \
     --build-arg=deploy_environment="$deploy_environment" \
     --build-arg=git_sha1="$git_sha1" \
